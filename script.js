a90b69174366f62259dd9dacc6afa42ed222b666
Naoman Saeed <naoman.saeed@gmail.com>
2025-12-29 05:44:21 +0500

    created a javascript fuction for button click

---
+++ script.js
@@ -0,0 +1,6 @@
+let saveBtn = document.getElementById("input-btn");
+
+//attach event listener function to save button
+saveBtn.addEventListener('click', () => {
+    console.log("button clicked");
+})
