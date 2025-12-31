67e3c4af7371ac0a7f459a207c5d4d06ccf2df72
Naoman Saeed <naoman.saeed@gmail.com>
2025-12-31 06:02:32 +0500

    minor edits

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 let myLeads = []
 
 const saveBtn = document.getElementById("input-btn");
-const inputEl = document.getElementById("input-el")
+const inputEl = document.getElementById("input-el");
+const listEl = document.getElementById("list-el");
 
 //attach event listener function to save button
 saveBtn.addEventListener('click', () => {
 
     myLeads.push(inputEl.value)
-    console.log(myLeads);
+    for(let i=0; i< myLeads.length; i++) {
+        listEl.innerHTML += "<li>" + myLeads[i] + "</li>"
+        //console.log(myLeads[i]);
+    }
+    
 })
