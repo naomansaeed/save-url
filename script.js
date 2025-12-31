7525d3790a2eaf760da9074bee2145f6a1ffaae3
Naoman Saeed <naoman.saeed@gmail.com>
2025-12-31 08:35:42 +0500

    using separate functions.

--- script.js
+++ script.js
@@ -6,17 +6,28 @@ const listEl = document.getElementById("list-el");
 
 //attach event listener function to save button
 saveBtn.addEventListener('click', () => {
-
     myLeads.push(inputEl.value)
-    for(let i=0; i< myLeads.length; i++) {
-        //listEl.innerHTML += "<li>" + myLeads[i] + "</li>"
-        //console.log(myLeads[i]);
-        // create element
-        const li = document.createElement("li");
-        //set text content
-        li.textContent = myLeads[i];
-        //appennd to the unordered list
-        listEl.append(li);
-    }
-    
+    appendList();
+    displayList();
 })
+
+// create a variable, listItems to hold all the HTML for the list Items.
+let listItems = "" ;
+
+function appendList() {
+    for(let i=0; i< myLeads.length; i++) {
+        listItems += "<li>" + myLeads[i] + "</li>"
+        //console.log(myLeads[i]);
+        
+        // create element
+        //const li = document.createElement("li");
+        //set text content
+        //li.textContent = myLeads[i];
+        //appennd to the unordered list
+        //listEl.append(li);
+    }
+}
+
+function displayList() {
+    listEl.innerHTML = listItems;
+}
